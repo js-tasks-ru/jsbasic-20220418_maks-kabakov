2c57e7cd87011efd323d0617d3585c6c2a22be9b
mac <mac@192.168.1.9>
2022-05-05 20:54:23 +0300

    функция валидации

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,9 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
-}
+function isValid(name){
+	if(!!name && !name.includes(' ') && name.length>=4)
+		return true;
+	return false;
+} 
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
