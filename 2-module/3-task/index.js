1900176713424604795c70e4bd554f0da07d49a8
mac <mac@192.168.1.8>
2022-05-15 19:01:14 +0300

    index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,23 @@
 let calculator = {
-  // ваш код
+a: +prompt('a?', 0),
+b: +prompt('b?', 0),
+
+read: function(a,b) {
+this.a = a;
+this.b = b;
+return [this.a, this.b];	
+},
+
+sum: function() {
+	return this.a + this.b;
+},
+
+mul: function() {
+	return this.a * this.b;
+},
+
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
